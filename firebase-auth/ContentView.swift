484cbeb13b0de7003bb9fa3dfc20a7dd4a034d33
shishimo <my_blue_wish@yahoo.co.jp>
2019-10-02 09:24:42 +0700

    created a project

---
+++ firebase-auth/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  firebase-auth
+//
+//  Created by Shinichiro Shimoda on 2019/10/02.
+//  Copyright © 2019 Shinichiro Shimoda. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ firebase-authTests/firebase_authTests.swift
@@ -0,0 +1,34 @@
+//
+//  firebase_authTests.swift
+//  firebase-authTests
+//
+//  Created by Shinichiro Shimoda on 2019/10/02.
+//  Copyright © 2019 Shinichiro Shimoda. All rights reserved.
+//
+
+import XCTest
+@testable import firebase_auth
+
+class firebase_authTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
